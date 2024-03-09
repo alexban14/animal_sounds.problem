f03328b31b105d393c9604867f22258ab5dff885
Alex Ban <alexbanut10@gmail.com>
2024-03-09 14:48:45 +0200

    refactored the loop that goes thru the collection of animals

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ animals.Add(new Dog("Dog"));
 animals.Add(new Cat("Cat"));
 animals.Add(new Cow("Cow"));
 
-for (int i = 0; i < animals.Count; i++)
+foreach (Animal animal in animals)
 {
-    Animal animal = animals[i];
     Console.WriteLine("{0} makes sound: {1}", animal.GetAnimalType(), animal.MakesSound());
 }
